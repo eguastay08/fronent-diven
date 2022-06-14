ff1a41a9ce1f6b29c82a18bb2aaca657ed794c9b
Eduardo Guastay <compunethomemx@gmail.com>
2022-06-14 15:20:11 -0500

    change csv generate

--- src/components/pages/surveys/index.jsx
+++ src/components/pages/surveys/index.jsx
@@ -233,8 +233,8 @@ const Surveys=(props)=>{
             const replacer = (key, value) => value === null ? '' : value
             const header = Object.keys(data[0])
             const csv = [
-              header.join(','),
-              ...data.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','))
+              header.join(';'),
+              ...data.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(';'))
             ].join('\r\n')
             const blob = new Blob([csv], { type: 'text/csv' });
             const url = window.URL.createObjectURL(blob)
