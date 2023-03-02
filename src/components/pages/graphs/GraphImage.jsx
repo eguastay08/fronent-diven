5ba288a7edae8dae9c4f88dbd0826b08eb483446
root <root@edwar.devsoftec.com>
2023-03-01 23:26:59 -0500

    view text img in size

--- src/components/pages/graphs/GraphImage.jsx
+++ src/components/pages/graphs/GraphImage.jsx
@@ -3,9 +3,10 @@ import {Modal} from "react-bootstrap";
 import ManageCharts from "./ManageCharts";
 import {useState} from "react";
 import style from "./style.module.scss";
+import GraphWordCloudChart from "./GraphWordCloudChart";
 
 const GraphImage=(props)=>{
-  const {question}=props
+  const {question, options}=props
   const [show, setShow] = useState(false);
   const API_URL = process.env.REACT_APP_API_URL;
 
@@ -17,8 +18,15 @@ const GraphImage=(props)=>{
   }
 
   return<>
-    <h3 className={styles.title}>{question.question}</h3>
-    <button onClick={handleShow} className="btn-diven btn-input btn btn-primary">Ver Fotografiás</button>
+    <GraphWordCloudChart
+      question={question}
+      options={options}
+      answers={0}
+      tot_answers={0}
+    />
+    <div style={{display:'flex',justifyContent:'center'}}>
+      <button onClick={handleShow} className="btn-diven btn-input btn btn-primary">Ver Fotografiás</button>
+    </div>
     <Modal
       fullscreen={true}
       show={show}
@@ -35,7 +43,7 @@ const GraphImage=(props)=>{
           <div className={style.container}>
             <div className={style.charts}>
               {
-                question?.data.map((e,i)=>{
+                question?.img.map((e,i)=>{
                   return <div className={style.chart}>
                     <img src={`${API_URL}/api/v1/image/${e?.image}`} style={{width:'100%', height:'400px'}}/>
                     <div>{e?.answer_txt}</div>
